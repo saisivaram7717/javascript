05398a8009fa87e03c31d047d005c9f46d5be061
sai@7717 <srikotisaisivaram@gmail.com>
2024-12-13 19:25:55 +0530

    task

---
+++ task7/task.js
@@ -0,0 +1,47 @@
+let card=document.getElementById('text');
+card.style.backgroundColor="orange";
+card.style.color="red"
+card.style.padding="30px"
+card.style.borderRadius="50px"
+card.style.cursor="pointer"
+
+
+function frist(){
+    card.textContent="SAI"
+    card.style.backgroundColor="blue"
+    card.style.textAlign="center"
+    card.style.padding="40px"
+    card.style.boxShadow="10px 10px 5px red" 
+}
+
+let card1=document.getElementById('content')
+card1.style.background="green"
+card1.style.color="purple"
+card1.style.padding="30px"
+card1.style.borderRadius="50px"
+card.style.cursor="pointer"
+function secound(){
+    card1.style.backgroundColor="pink"
+    card1.textContent="SIVA"
+    card1.style.textAlign="center"
+    card1.style.padding="40px"
+    card1.style.boxShadow="10px 10px 5px red" 
+
+}
+
+let card2=document.getElementById("content1")
+card2.style.backgroundColor="purple"
+card2.style.color="black"
+card2.style.padding="30px"
+card2.style.borderRadius="50px"
+card2.style.cursor="pointer"
+function third(){
+    card2.style.backgroundColor="brown"
+    card2.innerText="RAM"
+    card2.style.textAlign="center"
+    card2.style.padding="40px"
+    card2.style.boxShadow="10px 10px 5px green" 
+
+}
+
+
