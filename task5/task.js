632bce14074514b3aa9a3591280ce0b10b2536c8
sai@7717 <srikotisaisivaram@gmail.com>
2024-12-13 15:58:11 +0530

    task

---
+++ task5/task.js
@@ -0,0 +1,112 @@
+// Task:-- 
+// Write a program to print
+// 1-10
+// 10-1
+// -1 to -10
+// -10 to -1
+// Using while loop and
+// Print even numbers and odd numbers using while loop and take userInput and print sum of even and sum of odd numbers from userInput
+
+
+
+// for (i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+
+// i=1;
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+
+// i=11;
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=10);
+
+
+// for(i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+
+// for(i=-1;i>=-10;i--){
+//     console.log(i);
+// }
+
+// for(i=-10;i<=-1;i++){
+//     console.log(i)
+// }
+
+
+// Using while loop and
+// Print even numbers and odd numbers using while loop and take userInput and print sum of even and sum of odd numbers from userInput
+
+// i=1;
+// while(i<=20){
+//     if(i%2==0){
+//         console.log(i," is even number");
+//     }else{
+//         console.log(i," is odd number");
+//     }
+//     i++;
+// }
+
+// for(i=1;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i,"even number");
+//     }else{
+//         console.log(i,"odd number");
+//     }
+// }
+
+// for(i=1;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i,"even number");
+//     }
+// }
+
+// print the odd number
+
+// var userInput=prompt("enter the number");
+// var odd=userInput[0]
+// for(i=1;i<=userInput;i++){
+//     if(i%2==1){
+//         console.log(i,"is odd number");
+//     }
+// }
+
+
+
+// var n=prompt("enter the number");
+// i=1;
+// var evensum=0;
+// var oddsum=0;
+// while(i<=n){
+//     if(i%2==0){
+//         evensum+=i;
+//     }else{
+//         oddsum+=i;
+//     }
+//     i++;
+// }
+// console.log(evensum,"sum of even numbers");
+// console.log(oddsum,"sum of odd numbers")
+
+
+
+// Create a function where the function needs to have an object where obj consists of hero_name,movie_name,director_name,and plotof_movie and invoke the function and write a doc about that function working behind the scenes
+
+// function a(){
+//     hero={
+//        hero_name:"pavankalyan",
+//        movie_name:"kushi",
+//        director_name:"surya",
+//        plot_name:"blockbuster"
+//     }
+// console.log(hero)
+// }
+// a()
